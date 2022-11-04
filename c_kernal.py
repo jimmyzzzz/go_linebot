3f15278edfcd0bf021dfd067abe9427f6579feea
jimmyzzzz <sciencestudyjimmy@gmail.com>
2022-11-04 13:25:41 +0800

    kernal新增get_root_names

--- c_kernal.py
+++ c_kernal.py
@@ -7,32 +7,38 @@ import os
 
 class SampleCKernal(PercentageKernal):
 
-	def __init__(self, root_dir="CKernal_root", **kwargs):
-		super().__init__(root_dir=root_dir, **kwargs)
+    def __init__(self, root_dir="CKernal_root", **kwargs):
+        super().__init__(root_dir=root_dir, **kwargs)
 
-	def target_file_is_exist(self, root_name):
-		root_path = os.path.join(self.root_dir_path, f"{root_name}.so")
-		if os.path.isfile(root_path):
-			return True
-		return False
+    def get_self_root_names(self):
+        return [
+            file_name for file_name in os.listdir(self.root_dir_path)
+            if file_name[-3:]==".so"
+        ]
 
-	def import_root(self, root_name, option_list):
-		root_path = os.path.join(self.root_dir_path, f"{root_name}.so")
-		root = sample_c_root_wapper(root_name=root_name, root_path=root_path)
-		return root
+    def target_file_is_exist(self, root_name):
+        root_path = os.path.join(self.root_dir_path, f"{root_name}.so")
+        if os.path.isfile(root_path):
+            return True
+        return False
 
-	def run_command(self, CMD):
-		(RootOpt, CmdData) = self.get_RootOpt_CmdData(CMD)
-		(root_name, root, option_list) = RootOpt
-		(user_id, group_id, root_cmd, user_cmds) = CmdData
+    def import_root(self, root_name, option_list):
+        root_path = os.path.join(self.root_dir_path, f"{root_name}.so")
+        root = sample_c_root_wapper(root_name=root_name, root_path=root_path)
+        return root
 
-		return_str = root.run(user_id, group_id, root_cmd, user_cmds)
-		return return_str
+    def run_command(self, CMD):
+        (RootOpt, CmdData) = self.get_RootOpt_CmdData(CMD)
+        (root_name, root, option_list) = RootOpt
+        (user_id, group_id, root_cmd, user_cmds) = CmdData
+
+        return_str = root.run(user_id, group_id, root_cmd, user_cmds)
+        return return_str
 
 
 class CKernal(SampleCKernal):
 
-	def import_root(self, root_name, option_list):
-		root_path = os.path.join(self.root_dir_path, f"{root_name}.so")
-		root = c_root_wapper(root_name=root_name, root_path=root_path)
-		return root
+    def import_root(self, root_name, option_list):
+        root_path = os.path.join(self.root_dir_path, f"{root_name}.so")
+        root = c_root_wapper(root_name=root_name, root_path=root_path)
+        return root
