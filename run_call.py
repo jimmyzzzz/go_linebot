83e07e1240cd34a79d6b29bcbc80c49fe564753a
jimmyzzzz <sciencestudyjimmy@gmail.com>
2022-11-10 14:03:35 +0800

    [1]應用新版的取得uid的方式 [2]如果沒有root_cmd則解析結果為None

--- run_call.py
+++ run_call.py
@@ -1,53 +1,55 @@
 
 def root_cmds_preprocess(root_cmd):
-	#return [cmd.strip().split() for cmd in root_cmds]
-	return root_cmd.strip()
+    #return [cmd.strip().split() for cmd in root_cmds]
+    if root_cmd is None:
+        return root_cmd
+    return root_cmd.strip()
 
 def user_cmds_preprocess(user_cmds):
-	return [
-		tuple(c.strip() for c in cmd.split(':', 1))
-		for cmd in user_cmds if ':' in cmd
-	]
+    return [
+        tuple(c.strip() for c in cmd.split(':', 1))
+        for cmd in user_cmds if ':' in cmd
+    ]
 
 class RunCall:
 
-	def __init__(self, root_ref, user_id, group_id, root_cmd, user_cmds):
-		self.root = root_ref
-		self.run_index = 0
-		self.return_str_list = []
-		self.full_error = True
+    def __init__(self, root_ref, user_id, group_id, root_cmd, user_cmds):
+        self.root = root_ref
+        self.run_index = 0
+        self.return_str_list = []
+        self.full_error = True
 
-		self.user_id = user_id
-		self.group_id = group_id
-		self.root_cmd = root_cmds_preprocess(root_cmd)
-		self.user_cmds = user_cmds_preprocess(user_cmds)
+        self.user_id = user_id
+        self.group_id = group_id
+        self.root_cmd = root_cmds_preprocess(root_cmd)
+        self.user_cmds = user_cmds_preprocess(user_cmds)
 
-	def __setattr__(self, k, v):
-		static_vars = {"root", "return_str_list"}
-		static_vars |= {"user_id", "group_id", "root_cmds", "user_cmds"}
-		static_vars |= {"push_str", "get_return"}
-		if k == static_vars:
-			raise ValueError(f"{k} cannot overloading")
-		self.__dict__[k] = v
+    def __setattr__(self, k, v):
+        static_vars = {"root", "return_str_list"}
+        static_vars |= {"user_id", "group_id", "root_cmds", "user_cmds"}
+        static_vars |= {"push_str", "get_return"}
+        if k == static_vars:
+            raise ValueError(f"{k} cannot overloading")
+        self.__dict__[k] = v
 
-	def push_str(self, return_str):
-		self.return_str_list.append(return_str)
+    def push_str(self, return_str):
+        self.return_str_list.append(return_str)
 
-	def get_return(self):
-		join_str = ''.join(self.return_str_list)
-		ret_str = ''
-		backslash = False
+    def get_return(self):
+        join_str = ''.join(self.return_str_list)
+        ret_str = ''
+        backslash = False
 
-		for c in join_str:
-			if backslash:
-				if c=='n': ret_str+='\n'
-				elif c=='t': ret_str+='\t'
-				elif c=='\\': ret_str+='\\'
-				backslash=False
-				continue
-			if c=='\\':
-				backslash=True
-				continue
-			ret_str += c
+        for c in join_str:
+            if backslash:
+                if c=='n': ret_str+='\n'
+                elif c=='t': ret_str+='\t'
+                elif c=='\\': ret_str+='\\'
+                backslash=False
+                continue
+            if c=='\\':
+                backslash=True
+                continue
+            ret_str += c
 
-		return ret_str
+        return ret_str
--- short_call_kernal.py
+++ short_call_kernal.py
@@ -6,19 +6,28 @@ too_many_key_error = "[提示] 太多指令！"
 
 class ShortCallKernal(HalfKernal):
 
-	def get_self_root_names(self):
-		return ['uid', 'gid', 'py']
+    def get_self_root_names(self):
+        return ['lid', 'uid', 'gid', 'py']
 
-	def uid(self, CMD, CmdData):
-		(user_id, group_id, root_cmd, user_cmds) = CmdData
-		if root_cmd.strip(): return too_many_key_error
-		return user_id
+    def lid(self, CMD, CmdData):
+        (user_id, group_id, root_cmd, user_cmds) = CmdData
+        if root_cmd is not None: return too_many_key_error
+        return user_id
 
-	def gid(self, CMD, CmdData):
-		(user_id, group_id, root_cmd, user_cmds) = CmdData
-		if root_cmd.strip(): return too_many_key_error
-		return user_id
+    def uid(self, CMD, CmdData):
+        (user_id, group_id, root_cmd, user_cmds) = CmdData
+        if root_cmd is not None: return too_many_key_error
 
-	def py(self, CMD, CmdData):
-		(user_id, group_id, root_cmd, user_cmds) = CmdData
-		return str(eval(root_cmd))
+        success, uid = self.init_kernal_ref.pipe("get_uid", line_id=user_id)
+        if success:
+            return uid
+        return "UNKNOW"
+
+    def gid(self, CMD, CmdData):
+        (user_id, group_id, root_cmd, user_cmds) = CmdData
+        if root_cmd is not None: return too_many_key_error
+        return group_id
+
+    def py(self, CMD, CmdData):
+        (user_id, group_id, root_cmd, user_cmds) = CmdData
+        return str(eval(root_cmd))
