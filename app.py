c61c2e63905b65a396e10cb247ec2fd5f4a754c6
jimmyzzzz <sciencestudyjimmy@gmail.com>
2022-12-11 18:21:18 +0800

    加入quick button

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import QuickReply, QuickReplyButton, MessageAction
 
 app = Flask(__name__)
 
@@ -41,6 +42,28 @@ kernal = InitKernal()
 
 @handler.add(MessageEvent, message=TextMessage)
 def pretty_echo(event):
+
+	if event.message.text in {'$', '＄'}:
+
+		quick_reply = QuickReply(items=[
+			QuickReplyButton(action=MessageAction(label="help", text="$ help")),
+			QuickReplyButton(action=MessageAction(label="mod", text="$ mod")),
+			QuickReplyButton(action=MessageAction(label="ls", text="$ ls")),
+			QuickReplyButton(action=MessageAction(label="rels", text="$ rels")),
+			QuickReplyButton(action=MessageAction(label="uid", text="$ uid")),
+			QuickReplyButton(action=MessageAction(label="hello", text="$ hello")),
+			QuickReplyButton(action=MessageAction(label="upload", text="$ upload")),
+			QuickReplyButton(action=MessageAction(label="download", text="$ download")),
+		])
+
+		line_bot_api.reply_message(
+			event.reply_token,
+			TextSendMessage(
+				text='請選擇指令',
+				quick_reply=quick_reply
+			)
+		)
+		return
 
 	(is_cmd, return_txt) = kernal.run(event)
 
